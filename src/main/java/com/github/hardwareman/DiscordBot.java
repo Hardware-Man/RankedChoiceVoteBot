a4d6a47138821523d93bfddbf39de14fd0a6ab6f
Hardware-Man <kmishra686@gmail.com>
2021-03-27 17:20:27 -0400

    Initialize

---
+++ src/main/java/com/github/hardwareman/DiscordBot.java
@@ -0,0 +1,23 @@
+package com.github.hardwareman;
+
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+
+public class DiscordBot {
+    public static void main(String[] args) {
+        // Insert your bot's token here
+        String token = "ODI1NDU5MTcwNzY0NzgzNzE3.YF-Owg.YEtPgb_34ri1t9Cv3N_NgUrKgRU";
+
+        DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
+
+        // Add a listener which answers with "Pong!" if someone writes "!ping"
+        api.addMessageCreateListener(event -> {
+            if (event.getMessageContent().equalsIgnoreCase("!ping")) {
+                event.getChannel().sendMessage("Bong!");
+            }
+        });
+
+        // Print the invite url of your bot
+        System.out.println("You can invite the bot by using the following url: " + api.createBotInvite());
+    }
+}
