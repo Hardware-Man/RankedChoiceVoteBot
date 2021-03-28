19391ed0bd3a5fdf72fa252154411edf1e69e996
Hardware-Man <kmishra686@gmail.com>
2021-03-28 08:19:25 -0400

    PM commands

--- src/main/java/com/github/hardwareman/DiscordBot.java
+++ src/main/java/com/github/hardwareman/DiscordBot.java
@@ -24,8 +24,8 @@ public class DiscordBot {
 
         // Add a listener which answers with "Pong!" if someone writes "!ping"
         api.addMessageCreateListener(event -> {
+            StringTokenizer st = new StringTokenizer(event.getMessageContent());
             if (event.isServerMessage()) {
-                StringTokenizer st = new StringTokenizer(event.getMessageContent());
                 if (st.hasMoreTokens()) {
                     String tok = st.nextToken();
                     if (tok.equalsIgnoreCase("!votehelp")) {
@@ -33,13 +33,12 @@ public class DiscordBot {
                                 .setTitle("Command List")
                                 .setDescription("`!startvote [title] [candidate1],[candidate2],...` - Start a vote\n" +
                                         "`!endvote [electionid]` - End a running vote\n" +
-                                        "`!votestatus [electionid]` - Show a currently running election's status (Graphical Representation) \n" +
+                                        "`!votestatus [electionid]` - Show a currently running election's status (Graphical Representation)\n" +
                                         "`!getcandidates [electionid]` - Show a currently running election's Candidates")
                                 .setColor(new Color(245, 132, 66))
                                 .setThumbnail("https://i.ytimg.com/vi/P10PFuBFVL8/maxresdefault.jpg");
                         event.getChannel().sendMessage(embed);
-                    }
-                    else if (tok.equalsIgnoreCase("!startvote")) {
+                    } else if (tok.equalsIgnoreCase("!startvote")) {
                         EmbedBuilder embed = new EmbedBuilder();
                         if (st.hasMoreTokens()) {
                             tok = st.nextToken(" ");
@@ -48,8 +47,8 @@ public class DiscordBot {
                                     .limit(6)
                                     .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
                                     .toString();
-                            for(int j = 0; j < elections.size(); j++) {
-                                if(elections.get(j).getElectionID().equals(eID)) {
+                            for (int j = 0; j < elections.size(); j++) {
+                                if (elections.get(j).getElectionID().equals(eID)) {
                                     eID = new Random().ints(48, 123)
                                             .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
                                             .limit(6)
@@ -58,7 +57,7 @@ public class DiscordBot {
                                     j = 0;
                                 }
                             }
-                            elections.add(new Election(tok,eID));
+                            elections.add(new Election(tok, eID));
                             embed.setTitle(tok);
                             StringBuilder candidates = new StringBuilder();
                             if (st.hasMoreTokens()) {
@@ -73,7 +72,7 @@ public class DiscordBot {
                                     candidates.append("\n").append(tok);
                                 }
                                 embed.addField("Candidates", candidates.toString())
-                                        .addField("Election ID",eID)
+                                        .addField("Election ID", eID)
                                         .setThumbnail("https://i.ytimg.com/vi/P10PFuBFVL8/maxresdefault.jpg");
                                 event.getChannel().sendMessage(embed);
                             } else {
@@ -85,40 +84,68 @@ public class DiscordBot {
                                     .setDescription("Please state the election's title and at least one candidate.\n" +
                                             "Command format: `!startvote [title] [candidate1],[candidate2],...`"));
                         }
-                    }
-                    else if (tok.equalsIgnoreCase("!getcandidates")) {
+                    } else if (tok.equalsIgnoreCase("!getcandidates")) {
                         StringBuilder sb = new StringBuilder();
                         if (st.hasMoreTokens()) {
                             String eleID = st.nextToken();
-                            for(Election ele : elections) {
+                            for (Election ele : elections) {
                                 if (ele.getElectionID().equals(eleID)) {
-                                   for (Candidate candid : ele.candidates) {
-                                       sb.append(candid.getName()).append("\n");
-                                   }
+                                    for (Candidate candid : ele.candidates) {
+                                        sb.append(candid.getName()).append("\n");
+                                    }
                                 }
                             }
                             event.getChannel().sendMessage(new EmbedBuilder()
                                     .setTitle("Candidate List")
                                     .setDescription(sb.toString()));
                         }
-                    }
-                    else if (tok.equalsIgnoreCase("!endvote")) {
-                        if(st.hasMoreTokens()) {
+                    } else if (tok.equalsIgnoreCase("!endvote")) {
+                        if (st.hasMoreTokens()) {
                             String eleID = st.nextToken();
                             int eleIndex = -1;
-                            for(int i = 0; i < elections.size(); i++) {
+                            for (int i = 0; i < elections.size(); i++) {
                                 if (elections.get(i).getElectionID().equals(eleID)) {
                                     eleIndex = i;
                                     break;
                                 }
                             }
 
-                            if(eleIndex != -1) {
+                            if (eleIndex != -1) {
                                 Election selectedEle = elections.get(eleIndex);
                                 ArrayList<Candidate> candidates = selectedEle.candidates;
                                 ArrayList<Voter> voters = selectedEle.voters;
                                 //Work here Zach
                             }
+                        }
+                    }
+                }
+            } else {
+                if(st.hasMoreTokens()) {
+                    String tok = st.nextToken();
+                    if (tok.equalsIgnoreCase("!joinvote")) {
+                        if(st.hasMoreTokens()) {
+                            tok = st.nextToken();
+                            Election selectedEle = null;
+                            for(Election e : elections) {
+                                if(e.getElectionID().equals(tok)) {
+                                    selectedEle = e;
+                                }
+                            }
+                            if (selectedEle != null) {
+                                selectedEle.voters.add(new Voter(event.getMessageAuthor().getDiscriminatedName()));
+                                event.getChannel().sendMessage(new EmbedBuilder()
+                                        .setTitle("Joined Election Successfully")
+                                        .setDescription("You have joined the election: " + selectedEle.getElectionName() + "!"));
+                            } else {
+                                event.getChannel().sendMessage(new EmbedBuilder()
+                                        .setTitle("Invalid Join")
+                                        .setDescription("Election does not exist!"));
+                            }
+                        } else {
+                            event.getChannel().sendMessage(new EmbedBuilder()
+                                    .setTitle("Invalid Join")
+                                    .setDescription("Please state the election ID.\n" +
+                                            "Command format: `!joinvote [electionid]`"));
                         }
                     }
                 }
