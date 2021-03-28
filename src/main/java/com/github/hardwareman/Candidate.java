bebd0b9916515f8acd2de05d433b5fc302a8e0f3
Zach213 <78453852+Zach213@users.noreply.github.com>
2021-03-28 00:11:40 -0400

    jfoadosjifads

--- src/main/java/com/github/hardwareman/Candidate.java
+++ src/main/java/com/github/hardwareman/Candidate.java
@@ -1,19 +1,12 @@
 package com.github.hardwareman;
+import java.util.ArrayList;
 
 public class Candidate {
     private final String name;
-    private int rank = 0;
+    private ArrayList<Integer> rank = new ArrayList<>();
 
     public Candidate(String name) {
         this.name = name;
-    }
-
-    public void setRank(int rank) {
-        this.rank = rank;
-    }
-
-    public int getRank() {
-        return rank;
     }
 
     public String getName() {
--- src/main/java/com/github/hardwareman/Voter.java
+++ src/main/java/com/github/hardwareman/Voter.java
@@ -1,7 +1,10 @@
 package com.github.hardwareman;
 
+import java.util.ArrayList;
+
 public class Voter {
     private final String name;
+    private final ArrayList<String> rankings = new ArrayList<>();
 
     public Voter(String name) {
         this.name = name;
